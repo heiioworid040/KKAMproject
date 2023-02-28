d81a9b9221dc88adcdd4778d49c1657d55484905
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-28 16:12:42 +0900

    GOODS

---
+++ KKAMproject/src/main/java/com/kkam/goods/action/GoodsFrontController.java
@@ -0,0 +1,54 @@
+package com.kkam.goods.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kkam.goods.action.Action;
+import com.kkam.goods.action.ActionForward;
+
+public class GoodsFrontController extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+	
+
+	private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String sPath=req.getServletPath();
+		
+		ActionForward forward=null;
+		Action action=null;
+
+
+		if(sPath.equals("/login.kkam")) {
+			forward=new ActionForward();
+			forward.setPath("user/loginForm.jsp");
+			forward.setRedirect(false);
+
+		}
+
+
+		
+		/* 여기 위까지 if문 작성 */
+		
+		if(forward != null) {
+			if(forward.isRedirect()==true) {
+				resp.sendRedirect(forward.getPath());
+			}else {
+				RequestDispatcher dispatcher=req.getRequestDispatcher(forward.getPath());
+		        dispatcher.forward(req, resp);
+			}
+		}
+
+	}//doProcess()
+
+}//클래스
---
+++ KKAMproject/src/main/java/com/kkam/goods/db/GoodsDTO.java
@@ -0,0 +1,47 @@
+package com.kkam.goods.db;
+
+public class GoodsDTO {
+	private String G_code;
+	private String G_product;
+	private String G_img;
+	private String G_desc;
+	private String G_stop;
+	private int G_price;
+	
+	public String getG_code() {
+		return G_code;
+	}
+	public void setG_code(String g_code) {
+		G_code = g_code;
+	}
+	public String getG_product() {
+		return G_product;
+	}
+	public void setG_product(String g_product) {
+		G_product = g_product;
+	}
+	public String getG_img() {
+		return G_img;
+	}
+	public void setG_img(String g_img) {
+		G_img = g_img;
+	}
+	public String getG_desc() {
+		return G_desc;
+	}
+	public void setG_desc(String g_desc) {
+		G_desc = g_desc;
+	}
+	public String getG_stop() {
+		return G_stop;
+	}
+	public void setG_stop(String g_stop) {
+		G_stop = g_stop;
+	}
+	public int getG_price() {
+		return G_price;
+	}
+	public void setG_price(int g_price) {
+		G_price = g_price;
+	}
+}
