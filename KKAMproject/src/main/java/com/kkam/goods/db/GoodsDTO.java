d9173354c64ab0c00c3ac9c67b68be2506637e07
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-03-03 15:18:58 +0900

    [23.03.03 15:18] GoodsDTO 수정

--- KKAMproject/src/main/java/com/kkam/goods/db/GoodsDTO.java
+++ KKAMproject/src/main/java/com/kkam/goods/db/GoodsDTO.java
@@ -3,9 +3,10 @@ package com.kkam.goods.db;
 public class GoodsDTO {
 	private String G_code;
 	private String G_product;
+	private String G_imgS;
 	private String G_img;
 	private String G_desc;
-	private String G_stop;
+	private int G_stop;
 	private int G_price;
 	
 	public String getG_code() {
@@ -20,6 +21,12 @@ public class GoodsDTO {
 	public void setG_product(String g_product) {
 		G_product = g_product;
 	}
+	public String getG_imgS() {
+		return G_imgS;
+	}
+	public void setG_imgS(String g_imgS) {
+		G_imgS = g_imgS;
+	}
 	public String getG_img() {
 		return G_img;
 	}
@@ -32,10 +39,10 @@ public class GoodsDTO {
 	public void setG_desc(String g_desc) {
 		G_desc = g_desc;
 	}
-	public String getG_stop() {
+	public int getG_stop() {
 		return G_stop;
 	}
-	public void setG_stop(String g_stop) {
+	public void setG_stop(int g_stop) {
 		G_stop = g_stop;
 	}
 	public int getG_price() {
