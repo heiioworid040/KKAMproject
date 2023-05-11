a1b6d6bd2e864c2480a50aad001c8ba19174bb73
PC <PC@DESKTOP-RPTJ6AG>
2023-05-12 02:20:14 +0900

    230512 글쓰기(미완)

--- src/main/java/com/project/service/NoticeService.java
+++ src/main/java/com/project/service/NoticeService.java
@@ -1,10 +1,32 @@
 package com.project.service;
 
+import java.sql.Timestamp;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+
+import com.project.dao.NoticeDAO;
+import com.project.domain.NoticeDTO;
+
+@Service
 public class NoticeService {
+	
+	@Inject
+	private NoticeDAO noticeDAO;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void insertNotice(NoticeDTO noticeDTO) {
+		if(noticeDAO.getMaxNum()==null) {
+			noticeDTO.setN_num(1);
+		}else {
+			noticeDTO.setN_view(noticeDAO.getMaxNum()+1);
+		}
+		
+		noticeDTO.setN_view(0);
+		noticeDTO.setN_date(new Timestamp(System.currentTimeMillis()));
+		
+		noticeDAO.insertNotice(noticeDTO);
 	}
 
 }
